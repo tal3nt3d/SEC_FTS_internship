15a31ca3dc91dd0a4011590738789ba0c9e6561e
tal3nt3d <afonin05@yandex.ru>
2026-04-07 18:06:34 +0300

    added other funcs

--- app/main.py
+++ app/main.py
@@ -3,10 +3,7 @@
 from fastapi import FastAPI
 import uvicorn
 from config.settings import settings
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+from config.logger import logger
 
 app = FastAPI(title="Task Tracker API")
 
@@ -18,3 +15,7 @@ if __name__ == "__main__":
     print(f"Starting app in {settings.APP_ENV} mode")
     logger.info(f"Starting with settings: %s", settings.safe_settings)
     uvicorn.run("main:app", host=settings.APP_HOST, port=settings.APP_PORT, reload=settings.DEBUG)
+    
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
